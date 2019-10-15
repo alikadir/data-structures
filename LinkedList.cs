f0fffc9220a6bbd0cd20f5ca3163651a48c2ca34
Ali Kadir BAGCIOGLU <alikadir.bagcioglu@digiturk.com.tr>
2019-10-15 16:15:27 +0300

    init

---
+++ LinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataStructures
+{
+
+    public class Node
+    {
+        public string Name;
+        public DateTime BirthDate;
+        
+        public Node Next;
+    }
+    
+    public class LinkedList
+    {
+        public Node root = new Node();
+        
+        
+    }
+    
+    
+    
+}
